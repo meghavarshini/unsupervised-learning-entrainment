f5e01c8191a6d34668d9748a67228d5f92c4c943
Megh <meghavarshini@gmail.com>
2023-03-07 15:15:49 -0700

    testing fix

--- models/NED/plot_training_loss.py
+++ models/NED/plot_training_loss.py
@@ -12,6 +12,7 @@ def make_argument_parser():
     return parser
 
 def open_file(file_path):
+    print(file_path)
     with open(file_path, "r") as f: lines = [int(line.strip()) for line in f]
     print("number of data points: ", len(lines))
     return lines
