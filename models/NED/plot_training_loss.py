52cb19b5ebc29b3a2cce8bec808b5a52fcb6632b
Megh <meghavarshini@gmail.com>
2023-03-08 22:43:47 -0700

    aded dunctionality for plotting multiple lines

--- models/NED/plot_training_loss.py
+++ models/NED/plot_training_loss.py
@@ -17,10 +17,21 @@ def open_file(file_path):
     print("number of data points: ", len(lines))
     return lines
 
-def plot_data(y, plot_save_path):
-    x_points = [n for n, _ in enumerate(y)]
+def plot_data(y_dataset_list:list= ["tloss", "vloss"], plot_save_path,\
+              plot_title:str = "Loss Plot"):
+
+    x_points = [n for n, _ in enumerate(y[0])]
     print(len(x_points))
-    plot = plt.scatter(x_points, y)
+    for ls in y_dataset_list:
+        if len(y_dataset_list) == len(x_points) and len(x_points) is not None:
+            plot = plt.plot(x_points, ls)
+        else:
+            Raise Exception("your loss datasets do not have the same array size")
+
+    plt.title(plot_title)
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+
     plt.savefig(plot_save_path)
     plt.clf()
     print("plot saved to disk!")
@@ -32,10 +43,14 @@ if __name__ == "__main__":
     parser = make_argument_parser()
     args = parser.parse_args()
 
+    loss_data = [i for i in args.data_list.strip().split(',') if "loss" in i]
+    print(loss_data)
 
-
-    for _, i in enumerate(args.data_list.strip().split(',')):
+    # for _, i in enumerate(args.data_list.strip().split(',')):
+    for _, i in enumerate(loss_data):
         print("plotting data for: ", i)
         y_data = open_file(args.data_dir + "/" + i.strip())
-        plot = plot_data(y_data, args.data_dir + "/" + i.strip() + ".png")
+        plot = plot_data(y_data, \
+                         plot_save_path = args.data_dir + "/" + i.strip() + ".png",\
+                         plot_title= "Loss Data by Epoch")
         print("number of data points: ", len(y_data))
