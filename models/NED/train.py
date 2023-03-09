a03c02693ba5e414139386d1595a96dacf0c5332
John Culnan <jmculnan@email.arizona.edu>
2023-03-09 13:37:49 -0800

    comments added to train.py; updated data structure

--- models/NED/train.py
+++ models/NED/train.py
@@ -2,6 +2,7 @@ import os
 from ecdc import *
 from test import test, load_h5
 from matplotlib import pyplot as plt
+from datetime import datetime
 
 #------------------------------------------------------------------
 #Uncomment for parsing inputs
@@ -33,11 +34,10 @@ def model_setup(seed, cuda, data_directory, model_directory):
     if cuda:
         torch.cuda.manual_seed(seed)
 
-# kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
+    # kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
 
-# f = h5py.File('interaction_Fisherfeats.h5','r')
-# dataset=f['dataset']
-
+    # f = h5py.File('interaction_Fisherfeats.h5','r')
+    # dataset=f['dataset']
 
     dataset_id = 'Fisher_acoustic'  # applies for ip and op
     norm_id = 'nonorm'  # applies for ip and op
@@ -52,7 +52,6 @@ def model_setup(seed, cuda, data_directory, model_directory):
     else:
         print("model file not found")
 
-
     fdset = EntDataset(data_directory + '/train_' + dataset_id + '_' + norm_id +'.h5')
     train_loader = torch.utils.data.DataLoader(fdset, batch_size=128, shuffle=True)
 
@@ -64,49 +63,63 @@ def model_setup(seed, cuda, data_directory, model_directory):
     model = VAE().double()
     if cuda:
         model.cuda()
-#Model LR hasn't converged to a best loss in 100 epochs- so we make the LR biggers to make observations about convergence
-#We run the risk of the step size being too big
+    #Model LR hasn't converged to a best loss in 100 epochs- so we make the LR biggers to make observations about convergence
+    #We run the risk of the step size being too big
     optimizer = optim.Adam(model.parameters(), lr=0.01)
     #optimizer = optim.Adam(model.parameters(), lr=1e-3)
     return model, optimizer, train_loader, val_loader, model_name
 
+
 def train(each_epoch, model, train_loader, optimizer, cuda):
     print("training...")
+    # set model to training model
     model.train()
+    # set loss to 0
     train_loss = 0
-    for batch_idx, (data, y_data) in enumerate(train_loader):
-        data = Variable(data)
-#        print("data dimensions: ",data.size())
-        y_data = Variable(y_data)
- #       print("data dimensions: ", y_data.size())
-        #exit()
 
+    # for every batch in the dataloader
+    for batch_idx, (data, y_data) in enumerate(train_loader):
+        # convert xs and ys to torch tensors
+        data = torch.tensor(data, requires_grad=True)
+        y_data = torch.tensor(y_data, requires_grad=True)
+        # data = Variable(data)
+        # y_data = Variable(y_data)
+
+        # move to cuda if needed
         if cuda:
             data = data.cuda()
             y_data = y_data.cuda()
 
+        # zero gradient on optimizer
         optimizer.zero_grad()
 
-        # recon_batch = model(data)
         # VAE's forward function contains both encoder and decoder.
         # But for NED, only encoded items used for caculating distances
+        # feed xs through model to get reconstructed vectors
         recon_batch = model(data)
+        # calculate the loss
         loss = loss_function(recon_batch, y_data)
+        # backprop
         loss.backward()
+
         print("train loss calculation: ", batch_idx % args.log_interval)
-        #IndexError: invalid index of a 0-dim tensor.
+
         # Use `tensor.item()` in Python to convert a 0-dim tensor to a number
-        # todo: testing out changing .data for .item on loss calculations
-        # train_loss += loss.data
         train_loss += loss.item()
+
+        # update gradients
         optimizer.step()
+
+        # if it's time to log the training status
         if batch_idx % args.log_interval == 0:
             print(('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader),
-                # loss.data / len(data))))
                 loss.item() / len(data))))
-    train_loss /=  len(train_loader.dataset)
+
+    # divide loss by length of dataset
+    train_loss /= len(train_loader.dataset)
+
     print(('====> Epoch: {} Average loss: {:.4f}'.format(
           each_epoch, train_loss)))
 
@@ -115,7 +128,11 @@ def train(each_epoch, model, train_loader, optimizer, cuda):
 
 def validate(model, val_loader, cuda):
     print("processing validation set...")
+    time_before_setting_eval = datetime.now()
     model.eval()
+    time_after_setting_eval = datetime.now()
+    print(f"Setting model to eval took: {time_after_setting_eval - time_before_setting_eval}")
+
     val_loss = 0
     for i, (data, y_data) in enumerate(val_loader):
         if cuda:
@@ -135,6 +152,7 @@ def validate(model, val_loader, cuda):
     val_loss /= len(val_loader.dataset)
     print(('====> Validation set loss: {:.4f}'.format(val_loss)))
     return val_loss
+
 
 if __name__ == "__main__":
     parser = make_argument_parser()
