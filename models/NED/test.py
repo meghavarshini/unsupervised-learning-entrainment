2b21e250fd473a24a98d73e2631f5be5a49ca292
Megh <meghavarshini@gmail.com>
2023-03-21 11:50:30 -0700

    fixed test.py for 30 runs

--- models/NED/test.py
+++ models/NED/test.py
@@ -7,138 +7,138 @@ import os
 
 os.getcwd()
 
+
 def make_argument_parser():
-	parser = argparse.ArgumentParser(description='entrainment testing')
-	parser.add_argument('--no-cuda', action='store_true',
+    parser = argparse.ArgumentParser(description='entrainment testing')
+    parser.add_argument('--no-cuda', action='store_true',
                         default=False,
                         help='enables CUDA training')
-	parser.add_argument('--seed', type=int,
+    parser.add_argument('--seed', type=int,
                         default=1, metavar='S',
                         help='random seed (default: 1)')
-	parser.add_argument('--hff', default= "/home/tomcat/entrainment/NED_files/mini/test_Fisher_acoustic_nonorm.h5",
+    parser.add_argument('--hff', default="/home/tomcat/entrainment/NED_files/mini/test_Fisher_acoustic_nonorm.h5",
                         help='location of h5 file with the test data')
-	parser.add_argument('--model_name',
-                        default= "/home/tomcat/entrainment/NED_files/mini/models/trained_Fisher_acoustic_nonorm_l1_30dim.pt",
+    parser.add_argument('--model_name',
+                        default="/home/tomcat/entrainment/NED_files/mini/models/trained_Fisher_acoustic_nonorm_l1_30dim.pt",
                         help='name associated with the trained model')
-	# args = parser.parse_args()
-	return parser
+    # args = parser.parse_args()
+    return parser
+
 
 def load_h5(file):
-	print("loading h5 file: ", file)
-	file = h5py.File(file, 'r')
-	test = np.array(file['dataset'])
-	print("loading complete!")
-	return test
+    print("loading h5 file: ", file)
+    file = h5py.File(file, 'r')
+    test = np.array(file['dataset'])
+    print("loading complete!")
+    return test
 
 
 def test(X_test, model, p, cuda):
-	test_loss = 0
-	fake_test_loss = 0
-	Loss=[]
-	Fake_loss = []
+    test_loss = 0
+    fake_test_loss = 0
+    Loss = []
+    Fake_loss = []
 
-	# for batch_idx, (x_data, y_data) in enumerate(test_loader):
-	for idx, data in enumerate(X_test):
-		x_data = data[:228]
-		y_data = data[228:-1]
-		idx_same_spk =list(np.where(X_test[:,-1]==data[-1]))[0]
+    # for batch_idx, (x_data, y_data) in enumerate(test_loader):
+    for idx, data in enumerate(X_test):
+        x_data = data[:228]
+        y_data = data[228:-1]
+        idx_same_spk = list(np.where(X_test[:, -1] == data[-1]))[0]
 
-		ll = random.choice(list(set(idx_same_spk) -set([idx])))
-		spk = int(data[-1])
+        ll = random.choice(list(set(idx_same_spk) - set([idx])))
+        spk = int(data[-1])
 
-		x_data = Variable(torch.from_numpy(x_data))
-		y_data = Variable(torch.from_numpy(y_data))
+        x_data = Variable(torch.from_numpy(x_data))
+        y_data = Variable(torch.from_numpy(y_data))
 
-		y_fake_data = X_test[ll,228:-1]
+        y_fake_data = X_test[ll, 228:-1]
 
-		y_fake_data = Variable(torch.from_numpy(y_fake_data))
+        y_fake_data = Variable(torch.from_numpy(y_fake_data))
 
-		if cuda:
-			x_data = x_data.cuda()
-			y_data = y_data.cuda()
-			y_fake_data = y_fake_data.cuda()
+        if cuda:
+            x_data = x_data.cuda()
+            y_data = y_data.cuda()
+            y_fake_data = y_fake_data.cuda()
 
-		recon_batch = model(x_data)
+        recon_batch = model(x_data)
 
-		z_x = model.embedding(x_data)
-		z_y = model.embedding(y_data)
-		# z_x = x_data
-		# z_y = y_data
-		# loss_real = lp_distance(z_x, z_y, p).data
-		loss_real = lp_distance(z_x, z_y, p).item()
-		# loss_real = loss_function(z_x, z_y, mu, logvar)
+        z_x = model.embedding(x_data)
+        z_y = model.embedding(y_data)
+        # z_x = x_data
+        # z_y = y_data
+        # loss_real = lp_distance(z_x, z_y, p).data
+        loss_real = lp_distance(z_x, z_y, p).item()
+        # loss_real = loss_function(z_x, z_y, mu, logvar)
 
-		z_y_fake = model.embedding(y_fake_data)
-		# z_y_fake = y_fake_data
+        z_y_fake = model.embedding(y_fake_data)
+        # z_y_fake = y_fake_data
 
-		# loss_fake = lp_distance(z_x, z_y_fake, p).data
-		loss_fake = lp_distance(z_x, z_y_fake, p).item()
-		# loss_fake = loss_function(z_x, z_y_fake, mu, logvar)
+        # loss_fake = lp_distance(z_x, z_y_fake, p).data
+        loss_fake = lp_distance(z_x, z_y_fake, p).item()
+        # loss_fake = loss_function(z_x, z_y_fake, mu, logvar)
 
-		test_loss += loss_real
-		fake_test_loss += loss_fake
+        test_loss += loss_real
+        fake_test_loss += loss_fake
 
-		Loss.append(loss_real)
-		Fake_loss.append(loss_fake)
+        Loss.append(loss_real)
+        Fake_loss.append(loss_fake)
 
+    test_loss /= X_test.shape[0]
 
-	test_loss /= X_test.shape[0]
+    fake_test_loss /= X_test.shape[0]
 
-	fake_test_loss /= X_test.shape[0]
+    Loss = np.array(Loss)
+    Fake_loss = np.array(Fake_loss)
 
-	Loss=np.array(Loss)
-	Fake_loss=np.array(Fake_loss)
+    final_result = float(np.sum(Loss < Fake_loss)) / Loss.shape[0]
 
-	final_result = float(np.sum(Loss < Fake_loss)) / Loss.shape[0]
-
-	return test_loss, fake_test_loss, final_result
+    return test_loss, fake_test_loss, final_result
 
 
 def model_testing(model_name, X_test, cuda):
-	model = VAE().double()
+    model = VAE().double()
 
-	# todo: test out results on model without training
-	#   and then uncomment this
-	model = torch.load(model_name)
+    # todo: test out results on model without training
+    #   and then uncomment this
+    model = torch.load(model_name)
 
-	model.eval()
-	if cuda:
-		model.cuda()
+    model.eval()
+    if cuda:
+        model.cuda()
 
-	if 'l1' in model_name:
-		p=1
-	elif 'l2' in model_name:
-		p=2
-	else:
-		print("need better model name")
-		p=2
-		pdb
+    if 'l1' in model_name:
+        p = 1
+    elif 'l2' in model_name:
+        p = 2
+    else:
+        print("need better model name")
+        p = 2
+        pdb
 
-	test_loss, fake_loss, result = test(X_test, model, p, cuda)
+    test_loss, fake_loss, result = test(X_test, model, p, cuda)
 
-	print("Total Real Loss:" + str(test_loss) + "Total Fake Loss:" + str(fake_loss))
-	print(result)
+    print("Total Real Loss:" + str(test_loss) + "Total Fake Loss:" + str(fake_loss))
+    print(result)
 
-	return result
+    return result
 
 
 if __name__ == "__main__":
-	os.getcwd()
-	SEED=448
-	parser = make_argument_parser()
-	args = parser.parse_args()
-	args.cuda = not args.no_cuda and torch.cuda.is_available()
+    os.getcwd()
+    SEED = 448
+    parser = make_argument_parser()
+    args = parser.parse_args()
+    args.cuda = not args.no_cuda and torch.cuda.is_available()
 
-	torch.manual_seed(args.seed)
+    torch.manual_seed(args.seed)
 
-	if not args.no_cuda and torch.cuda.is_available():
-		torch.cuda.manual_seed(args.seed)
+    if not args.no_cuda and torch.cuda.is_available():
+        torch.cuda.manual_seed(args.seed)
 
-	X_test1 = load_h5(args.hff)
-	results = []
-	for i in range(1, 31):
-		print("run: ", i)
-		test_run = model_testing(args.model_name, X_test1, args.cuda)
-		results.append(test_run)
-	print(mean(results))
-
+    X_test1 = load_h5(args.hff)
+    results = []
+    for i in range(1, 31):
+        print("run: ", i)
+        test_run = model_testing(args.model_name, X_test1, args.cuda)
+        results.append(test_run)
+    print("mean classification accuracy: ", np.mean(np.array(results)))
